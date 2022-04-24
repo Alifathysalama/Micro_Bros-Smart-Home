5731bd18f2bb36b388f8c8246faa95bbf5508933
Alifathysalama <alifathy996@gmail.com>
2022-04-24 07:27:02 +0200

    Added the KeyPad header file

--- Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/Debug/main.d
+++ Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/Debug/main.d
@@ -12,7 +12,8 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ .././KeyPad.h .././open_door.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -41,3 +42,7 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+.././KeyPad.h:
+
+.././open_door.h:
--- Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/main.c
+++ Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/main.c
@@ -7,7 +7,8 @@
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
-
+#include "KeyPad.h" // KeyPad has only one function Get_Key() that takes no i/p and return the pressed key 
+#include "open_door.h" 
 
 int main(void)
 {
