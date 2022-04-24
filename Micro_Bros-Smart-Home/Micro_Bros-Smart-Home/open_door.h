54d9f23eeaea2eee9207e2e9d5e132150da84665
SaadElnagar <saad.elnagar318@gmail.com>
2022-04-24 07:17:26 +0200

    add opendoor function for servo motor

---
+++ Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/open_door.h
@@ -0,0 +1,29 @@
+﻿/*
+ * open_door.h
+ *
+ * Created: 24/04/2022 06:55:06 ص
+ *  Author: SaadElnagar
+ */ 
+
+
+#ifndef OPEN_DOOR_H_
+#define OPEN_DOOR_H_
+#define PD5 PORTD5
+
+void opendoor ()
+{
+	DDRD |= (1<<PD5);	/* Make OC1A pin as output */
+	TCNT1 = 0;		/* Set timer1 count zero */
+	ICR1 = 2499;		/* Set TOP count for timer1 in ICR1 register */
+	/* Set Fast PWM, TOP in ICR1, Clear OC1A on compare match, clk/64 */
+	TCCR1A = (1<<WGM11)|(1<<COM1A1);
+	TCCR1B = (1<<WGM12)|(1<<WGM13)|(1<<CS10)|(1<<CS11);
+	OCR1A = 65;	/* Set servo shaft at -90° position */
+	_delay_ms(1500);
+	OCR1A = 175;	/* Set servo shaft at 0° position */
+	_delay_ms(1500);
+}
+
+
+
+#endif /* OPEN_DOOR_H_ */
