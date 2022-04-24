c24d6b65a94135c94a7b96ffe455f6e74bd7a191
karim ragab <85512359+kudu-G@users.noreply.github.com>
2022-04-24 20:17:30 +0200

    Add files via upload
    has one function

--- Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/temp.h
+++ Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/temp.h
@@ -12,5 +12,14 @@
   ADCSRA = 0x87;          /* Enable ADC, with freq/128  */
   ADMUX = 0x40;           /* Vref: Avcc, ADC channel: 0 */
 
+  int ADC_Read(char channel)
+  {
+	  ADMUX = 0x40 | (channel & 0x07);   /* set input channel to read */
+	  ADCSRA |= (1 << ADSC);               /* Start ADC conversion */
+	  while (!(ADCSRA & (1 << ADIF)));     /* Wait until end of conversion by polling ADC interrupt flag */
+	  ADCSRA |= (1 << ADIF);               /* Clear interrupt flag */
+	  _delay_ms(1);                      /* Wait a little bit */
+	  return ADCW;                       /* Return ADC word */
+  }
 
 #endif
