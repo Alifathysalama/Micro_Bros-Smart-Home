27f3e6be1c84fe43c0b6f50bd5e1b627c399e436
Alifathysalama <alifathy996@gmail.com>
2022-04-24 02:43:52 +0200

    The main Program

---
+++ Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/main.c
@@ -0,0 +1,19 @@
+/*
+ * Micro_Bros-Smart-Home.c
+ *
+ * Created: 4/24/2022 2:37:59 AM
+ * Author : Micro_Bros
+ */ 
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+    
+    while (1) 
+    {
+    }
+}
+
