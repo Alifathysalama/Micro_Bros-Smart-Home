fe8616ee2a2c227afa223113486a62637860e78f
karim ragab <85512359+kudu-G@users.noreply.github.com>
2022-04-24 18:50:21 +0200

    Update temp.h

--- Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/temp.h
+++ Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/temp.h
@@ -3,22 +3,17 @@
 4:32 PM
 */
 #define F_CPU 8000000UL
-#include <util/delay.h>
-#include <string.h>
-#include <stdio.h>
-#include "LCD_16x2_H_file.h"
-
 #define degree_sysmbol 0xdf
 
 
-void ADC_Init(){
+void Temp_sensor_Init(){
 	DDRA &=~(1<<0)	        /* Make ADC portA0 as input */
 	DDRD |=(1<<1)           //make port D1 output
 	ADCSRA = 0x87;          /* Enable ADC, with freq/128  */
 	ADMUX = 0x40;           /* Vref: Avcc, ADC channel: 0 */
 }
 
-int ADC_Read(char channel)
+int Temp_Read(char channel)
 {
 	ADMUX = 0x40 | (channel & 0x07);   /* set input channel to read */
 	ADCSRA |= (1<<ADSC);               /* Start ADC conversion */
