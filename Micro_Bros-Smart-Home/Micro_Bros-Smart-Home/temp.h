e616b16ce31f40027d7f7c6f43a0b9acce0ed6e0
karim ragab <85512359+kudu-G@users.noreply.github.com>
2022-04-24 19:42:19 +0200

    Add files via upload

--- Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/temp.h
+++ Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/temp.h
@@ -2,23 +2,15 @@
 24/4/2022
 4:32 PM
 */
-#define F_CPU 8000000UL
+
+#ifndef temp_H_
+#define temp_H_
 #define degree_sysmbol 0xdf
 
+   DDRA &=~(1<<0)	        /* Make ADC portA0 as input */
+  DDRD |=(1<<1)           //make port D1 output
+  ADCSRA = 0x87;          /* Enable ADC, with freq/128  */
+  ADMUX = 0x40;           /* Vref: Avcc, ADC channel: 0 */
 
-void Temp_sensor_Init(){
-	DDRA &=~(1<<0)	        /* Make ADC portA0 as input */
-	DDRD |=(1<<1)           //make port D1 output
-	ADCSRA = 0x87;          /* Enable ADC, with freq/128  */
-	ADMUX = 0x40;           /* Vref: Avcc, ADC channel: 0 */
-}
 
-int Temp_Read(char channel)
-{
-	ADMUX = 0x40 | (channel & 0x07);   /* set input channel to read */
-	ADCSRA |= (1<<ADSC);               /* Start ADC conversion */
-	while (!(ADCSRA & (1<<ADIF)));     /* Wait until end of conversion by polling ADC interrupt flag */
-	ADCSRA |= (1<<ADIF);               /* Clear interrupt flag */
-	_delay_ms(1);                      /* Wait a little bit */
-	return ADCW;                       /* Return ADC word */
-}
+#endif
