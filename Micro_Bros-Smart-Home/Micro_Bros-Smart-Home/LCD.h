17bff056cb7d1376f9f72019847dff8b5d73c7fc
AbdelrhmanEladawy <86054327+AbdelrhmanEladawy@users.noreply.github.com>
2022-04-25 14:37:16 +0200

    Add LCD files

---
+++ Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/LCD.h
@@ -0,0 +1,150 @@
+/*
+ * LCD.h
+ *
+ *  Created on: Apr 24, 2022
+ *      Author: abdelrhman
+ */
+
+/*
+ *	LCD is (Liquid Crystal Displays) are used for displaying status or
+ *	parameters in embedded systems.
+ *	it is 2 rows (0:1) and 16 column (0:15)
+		._______________________________________________.
+		|												|
+		|												|
+		|					LCD 16*2					|
+		|												|
+		|												|
+		|_______________________________________________|
+		   | | |	|  |  |		| | | | | | | | 	| |
+		   V V V	R  R  E		D D D D D D D D		L L
+		   S C E	S  W		0 1 2 3 4 5 6 7		E E
+		   S C E									D D
+		   	   	   	   	   	   	   	   	   	   	   	+ -
+
+connection | | |	|  |  |		| | | | | | | |		| |
+    to	   | | |	|  |  |		| | | | | | | |		| |
+    our	   | | |	|  |  |		| | | | | | | |		| |
+ ATmega16 31 30 29 16  17 21	29 28 ....... 22
+
+ 	 	  G	 5		P  P  P		P P ...       P		5 G
+ 	 	  N	 V		D  D  D		C C ...		  C		v N
+ 	 	  D			2  3  7		7 6 ...		  0		  D
+
+* RS    : register select	 --> 1: data   , 0: command
+* RW    : read/write  		 --> 1: write  , 0: read
+* E	    : enable			 --> 1: enable , 0: disable
+* D0-D7 : takes data or code
+* VSS   : to ground
+* VCC   : to source
+* VEE   : screen brightness
+*/
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+enum LCD_command
+{
+	LCD_clear_screen 	 		= 0x01 ,
+	LCD_auto_increment_cursor 	= 0x06 ,
+	LCD_display_on_cursor_off 	= 0X0C,
+	LCD_move_cursor_left 		= 0X10 ,
+	LCD_move_cursor_right 		= 0x14 ,
+	LCD_shift_entire_left 		= 0x18,
+	LCD_shift_entire_right 		= 0x1c,
+	LCD_set_mode_8bit_2r 		= 0x38 ,
+	LCD_set_cursor_r0 			= 0X80 ,
+	 	 	 	 	 	 //set cursor to row 0 ,then set the position with "|"
+	LCD_set_cursor_r1 			= 0xC0
+						//set cursor to row 1 ,then set the position with "|"
+};
+
+
+void LCD_send_command( uint8_t command )
+{
+	PORTD &= ~(1<<2);	//set RS to 0 to send command
+	PORTD &= ~(1<<3); 	//set RW t0 0 to write
+	_delay_ms(20);
+	PORTD |= (1<<7);	//enable LCD by set E
+	_delay_ms(20);
+	PORTC = command;	 //write the command to port
+	_delay_ms(20);
+	PORTD &= ~(1<<7);	//disable LCD by reset E
+	_delay_ms(10);
+}
+
+
+void LCD_initialize()
+{
+	DDRC = 0xff; 						//make all port as output
+	DDRD = 0b10001100;					//make RS,RW,E as output
+	LCD_send_command(LCD_set_mode_8bit_2r);	//set the mode to 8 bit , 2 row
+	LCD_send_command(LCD_display_on_cursor_off);
+	LCD_send_command(LCD_auto_increment_cursor);
+	LCD_send_command(LCD_clear_screen);
+	LCD_send_command(LCD_set_cursor_r0 | 0 ); //set Cursor to home position
+}
+
+//row is in range (0:1)  .... pos is in range (0:15)
+//if it is out of range ... it will print with current position
+void LCD_set_Cursor( uint8_t row , uint8_t pos )
+{
+	if (row == 0 && pos<16)
+		LCD_send_command((pos & 0x0F) | LCD_set_cursor_r0);
+	else if (row == 1 && pos<16)
+		LCD_send_command((pos & 0x0F) | LCD_set_cursor_r1);
+}
+
+//display data on LCD
+void LCD_display_char( uint8_t data )
+{
+	PORTD |=  (1<<2); 	//set RS to 1 to send data
+	PORTD &= ~(1<<3);	//set RW t0 0 to write
+	_delay_ms(20);
+	PORTD |= (1<<7);	//enable LCD by set E
+	_delay_ms(20);
+	PORTC = data;		//write data to port (set data)
+	_delay_ms(20);
+	PORTD &= ~(1<<7);	//disable LCD by reset E
+	_delay_ms(10);
+}
+
+void LCD_display_text( uint8_t *str , double delay  )
+{
+	uint8_t i;
+	for(i = 0 ; str[i] ; i++ )
+	{
+		LCD_display_char( str[i] );
+		_delay_ms(delay);
+	}
+}
+
+//dir : direction of shift ---> 0: left , 1: right
+//num_shift : how many time you want to shift
+// delay : the delay between every shift in ms
+void LCD_shift_entire_display( uint8_t dir , uint8_t num_shift , double delay)
+{
+	uint8_t i;
+	for(i = 0 ; i< num_shift ; i++)
+	{
+		if(dir == 0)
+		{
+			LCD_send_command(LCD_shift_entire_left);/* shift entire display left */
+			_delay_ms(delay);
+		}
+		else
+		{
+			LCD_send_command(LCD_shift_entire_right);/* shift entire display right */
+			_delay_ms(delay);
+		}
+	}
+}
+
+void LCD_clear()
+{
+	LCD_send_command(LCD_clear_screen);
+	LCD_send_command(LCD_set_cursor_r0 | 0 ); //set Cursor to home postion
+}
+
+#endif /* LCD_H_ */
--- Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/main.c
+++ Micro_Bros-Smart-Home/Micro_Bros-Smart-Home/main.c
@@ -10,10 +10,59 @@
 #include "KeyPad.h" // KeyPad has only one function Get_Key() that takes no i/p and return the pressed key 
 #include "open_door.h" 
 #include "temp.h"   //temp.h has two functions ADC_init() & ADC_Read()
+#include "LCD.h"
+/*
+********************   LCD functions   ********************
+ * LCD_initialize() : to  initialize the LCD
+ 		takes no parameters and return Void
+ 		it is set Cursor to home position
+ 		it is auto increment cursor
 
+ *- LCD_send_command( uint8_t command ) : to do an event
+		takes 8 bit (in command section below) number and return Void.
+
+ *- LCD_set_Cursor( uint8_t row , uint8_t pos )
+		takes : row is in range (0:1)  .... pos is in range (0:15)
+		and return Void.
+		if it is out of range ... it will be at current position
+
+ *- LCD_display_char( uint8_t data )
+		takes 8 bit char and print it
+
+ *- LCD_display_text( uint8_t *str , uint16_t delay  )
+		takes pointer to char (array of char) and print it
+		delay is time between every char in ms
+
+ *- LCD_shift_entire_display( uint8_t dir , uint8_t num_shift , uint16_t delay)
+		takes
+		dir : direction of shift ---> 0: left , 1: right
+		num_shift : how many time you want to shift
+		delay : the delay between every shift in ms
+		and return void.
+
+*- LCD_clear() : to clear the hole screen and return to home position
+		takes no parameters and return Void
+
+********************   LCD commands   ********************
+	LCD_clear_screen 	 		= 0x01 ,
+	LCD_auto_Increment_cursor 	= 0x06 ,
+	LCD_display_on_cursor_off 	= 0X0C,
+	LCD_move_cursor_left 		= 0X10 ,
+	LCD_move_cursor_right 		= 0x14 ,
+	LCD_shift_entire_left 		= 0x18,
+	LCD_shift_entire_right 		= 0x1c,
+	LCD_set_mode_8bit_2r 		= 0x38 ,
+	LCD_set_Cursor_r0 			= 0X80 ,
+	 	 	 	 	 	 //set cursor to row 0 ,then set the position with "|"
+	LCD_set_Cursor_r1 			= 0xC0
+						//set cursor to row 1 ,then set the position with "|"
+*/
 
 int main(void)
 {
+	LCD_initialize();
+
+
   	//there is no need for making fun. in this part of pir motion sensor as it will be as interrupt so i didn't do header file..
   	DDRA= ~(1<<1); // define pin A1 as input
   	DDRD |= (1<<6) ; // define pin D6 as led output
@@ -24,7 +73,14 @@ int main(void)
 	  	else
 	  	PORTD &=~(1<<6);
 	  	
-	  	
+	  	//test LCD
+		LCD_display_text("Welcome Home , ",300);
+		_delay_ms(1000);
+		LCD_set_Cursor(1,0);
+		LCD_display_text("Mr. abdelrhman",300);
+		_delay_ms(1000);
+		LCD_shift_entire_display(0, 3 , 500);
+		LCD_shift_entire_display(1, 3 , 500);
   	}
  }
 
